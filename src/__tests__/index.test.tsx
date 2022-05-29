819ff7eea8d12a6bf5c0d617b617b3b7f44f7dc9
floydkim <for12gm@gmail.com>
2022-05-29 23:01:49 +0900

    migrate component render test

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,1 +1,41 @@
-it.todo('write a test');
+import React from 'react';
+import { View, Image, Text, StyleSheet } from 'react-native';
+import RollingBar from '../index';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+
+describe('test', () => {
+  it('renders correctly', () => {
+    renderer.create(
+      <RollingBar interval={1000} customStyle={{ height: 45 }}>
+        <View style={{ flexDirection: 'row' }}>
+          <Image
+            style={{ width: 29, height: 29, marginRight: 10 }}
+            source={{
+              uri: 'https://facebook.github.io/react-native/img/tiny_logo.png',
+            }}
+          />
+          <Image
+            style={{ width: 29, height: 29, marginRight: 10 }}
+            source={{
+              uri: 'https://img.icons8.com/flat_round/64/000000/wide-long-right-arrow.png',
+            }}
+          />
+          <Text style={styles.sectionTitle}>Network Image</Text>
+        </View>
+        <View style={{ alignItems: 'center' }}>
+          <Text style={{ fontSize: 17 }}>SOME NOTICE HERE</Text>
+        </View>
+      </RollingBar>
+    );
+  });
+});
+
+const styles = StyleSheet.create({
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: '#444',
+  },
+});
